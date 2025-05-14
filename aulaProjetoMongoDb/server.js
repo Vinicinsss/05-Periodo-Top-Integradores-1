ca310f738bca07b8c430c2edc8b39363f82c00be
Vinicinsss <143666494+Vinicinsss@users.noreply.github.com>
2025-05-14 00:56:51 +0000

    Aula dia 13/05/2025

--- aulaProjetoMongoDb/server.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from './app.js';
-const PORT = process.env.PORT || 3000;
- createApp()
-    .then(app => app.listen(PORT, () => {
-            console.log(`Servidor rodando na porta ${PORT}`);
-        }))
-        .catch(err => {
-            console.error('Erro ao iniciar o servidor:', err);
-        });
